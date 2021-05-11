a9d8776a375a4ce0bd0441684889f36de2b8d515
Mohmed Anis <anishagwan@gmail.com>
2021-05-12 02:08:28 +0530

    question and answer func

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,44 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex += 1;
+    print(questionIndex);
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What\'s your fav color?",
+      "What\'s your fav animal?",
+    ];
+
     return MaterialApp(
       title: 'Quizine',
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quizine"),
         ),
-        body: Text("Hello, Friend"),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            ElevatedButton(
+                onPressed: answerQuestion,
+                child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 3"),
+            )
+          ],
+        ),
       ),
     );
   }

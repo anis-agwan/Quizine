be28bbf7f4180d53d336845c822b9c1294a3a4f1
Mohmed Anis <anishagwan@gmail.com>
2021-05-15 16:13:27 +0530

    Created the answer widget with callback func as argument

---
+++ lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: selectHandler,
+        child: Text("Answer 1"),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+          //onPrimary: Colors.blue
+        ),
+      ),
+    );
+  }
+}

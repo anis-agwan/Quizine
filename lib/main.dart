98f4e0bc01cc52f6498c8a1066319c5044bdb17a
Mohmed Anis <anishagwan@gmail.com>
2021-05-23 16:31:20 +0530

    Added reset quiz button

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,15 @@ class _MyAppState extends State<MyApp> {
     },
   ];
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
-
     setState(() {
       _questionIndex += 1;
     });
@@ -58,7 +64,7 @@ class _MyAppState extends State<MyApp> {
         body:
         _questionIndex < _questions.length
             ? Quiz(answerQuestion: _answerQuestion, questionIndex: _questionIndex, questions: _questions,)
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -2,13 +2,48 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText = "You did it, No more questions";
+    if (resultScore <= 8) {
+      resultText = "You are awesome and innocent";
+    } else if (resultScore <= 12) {
+      resultText = "Pretty likeable";
+    } else if (resultScore <= 16) {
+      resultText = "You are ... strange?";
+    } else {
+      resultText = "You are wicked";
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text("No more questions")
+        child:
+        Column(
+          children: [
+            Text(
+              resultPhrase,
+              style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+                onPressed: resetHandler,
+                child: Text("Restart the Quiz"),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.blue
+                ),
+            )
+          ],
+        )
     );
   }
 }
